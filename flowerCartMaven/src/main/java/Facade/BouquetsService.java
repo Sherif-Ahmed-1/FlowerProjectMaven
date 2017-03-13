44957759a126a99cbb77f0d6031d8c16d979beea
AllaaAmer <lolitabadr94@gmail.com>
2017-03-13 12:20:56 +0200

    Home new UI

---
+++ flowerCartMaven/src/main/java/Facade/BouquetsService.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Facade;
+
+import Entities.Product;
+import dao.ProductDoa;
+import java.util.ArrayList;
+/**
+ *
+ * @author alaa
+ */
+public class BouquetsService {
+    
+    public ArrayList<Product> getBouquetsFormFlower(int id )
+    {
+        ProductDoa pDao = new ProductDoa();
+        return pDao.selectProductsByFlower(id);
+    }  
+    
+    public ArrayList<Product> getBouquetsFormCategory(int id )
+    {
+        ProductDoa pDao = new ProductDoa();
+        return pDao.selectProductsByCategory(id);
+    }  
+}
---
+++ flowerCartMaven/src/main/java/servlets/CategoryBouquetsServlet.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import Entities.Product;
+import Facade.BouquetsService;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author alaa
+ */
+@WebServlet(name = "CategoryBouquetsServlet", urlPatterns = {"/categoryBouquets"})
+public class CategoryBouquetsServlet extends HttpServlet {
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+         int id = Integer.parseInt(request.getParameter("c"));
+        BouquetsService service = new BouquetsService();
+        ArrayList<Product> bouquetscat = service.getBouquetsFormCategory(id);
+        
+        request.setAttribute("bouquetscat", bouquetscat);
+        RequestDispatcher dis = request.getRequestDispatcher("BouquetsCategory.jsp");
+        dis.forward(request, response);
+        
+    }
+
+   
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
+
+}
